7457747e8fabc14c9e50cd1e0f900a8e70c8bfe7
seoyeonko <seoyeon99o@naver.com>
2021-10-26 16:56:19 +0900

    map, concat, filter 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import Event from './Event';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [food, setFood] = useState([
+    {id: 1, text: '삼겹살'},
+    {id: 2, text: '아이스크림'},
+    {id: 3, text: '회덮밥'},
+    {id: 4, text: '돈까스'},
+  ])
+
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+  
+  const onChnage = e => setInputText(e.target.value);
+  const onClick = () => {
+    const nextFood = food.concat({
+      id: nextId, 
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setFood(nextFood);
+    setInputText('');
+  };
+  const onRemove = (id) => {
+    // 더블클릭된 아이템을 제외한 나머지 리스트를 저장 
+    const nextFood = food.filter(f => f.id !== id)
+    setFood(nextFood);
+  }
+
+  const foodList = food.map((f) => <li key={f.id} onDoubleClick={()=>onRemove(f.id)}>{f.id + ":" +f.text}</li>)
+
   return (
     <>
-      <Event />
+      <input type="text" value={inputText} onChange={onChnage} />
+      <button onClick={onClick}>추가</button>
+      {foodList}
     </>
   );
 };
+
 
 export default App;
---
+++ src/Repeat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Repeat = () => {
+  return (
+    <div>
+      <li>삼겹살</li>
+      <li>회덮밥</li>
+      <li>아이스크림</li>
+      <li>초콜릿</li>
+    </div>
+  );
+};
+
+export default Repeat;
